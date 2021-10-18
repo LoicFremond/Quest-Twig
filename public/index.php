809af9d979ea11082eb582f9194adf25f65fab57
Loic Fremond <loic.fremond@hotmail.fr>
2021-10-18 13:55:30 +0200

    quete

--- public/index.php
+++ public/index.php
@@ -7,3 +7,9 @@ $loader = new Twig\Loader\FilesystemLoader( __DIR__.'/../src/View');
 $twig = new Twig\Environment($loader, []);
 
 echo $twig->render('index.html.twig', ['products' => $products]);
+
+$twig = new \Twig\Environment($loader, [
+    'debug' => true,
+    // ...
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
